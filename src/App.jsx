75f0c658c22cc88b7e3f8f4cfdfa5c66b30567a7
abhishek.sankhla <abhisheksankhl121@gmail.com>
2025-12-18 11:36:41 +0530

    basic clean up in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import LoginCards from "./components/LoginCards";
 import LoginPage from "./components/LoginPage"
 import RegisterPage from "./components/RegisterPage"
-import AdminPage from "./components/AdminPage"
-import HomePage from "./components/HomePage"
-import Cart from "./components/Cart"
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 import AdminDashboard from "./components/AdminDashboard";
 import DeliveryDashboard from "./components/DeliveryDashboard";
@@ -25,9 +22,6 @@ function App() {
       <Route path="/" element={<LoginCards/>}/>
       <Route path="/login" element={<LoginPage/>}/>
       <Route path="/register" element={<RegisterPage/>}/>
-      <Route path="/admin" element={<AdminPage/>}/>
-      <Route path="/Home" element={<HomePage/>}/>
-      <Route path="/Cart" element={<Cart/>}/> 
       <Route path="/customer/order" element={<RoleRoute allowedRoles={["CUSTOMER"]}>
                 <CustomerOrder/>
               </RoleRoute>}/>
